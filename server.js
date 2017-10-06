2b95cabdab0a7ad9d72c6ef7a4d8cd56dbcbc669
case2912 <shiroyama@maia.eonet.ne.jp>
2017-10-06 11:50:40 +0900

    update

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
+function getLocalAddress() {
+  var ifacesObj = {}
+  ifacesObj.ipv4 = [];
+  var interfaces = require('os').networkInterfaces();
+  for (var dev in interfaces) {
+    interfaces[dev].forEach(function(details) {
+      if (!details.internal) {
+        switch (details.family) {
+          case "IPv4":
+            ifacesObj.ipv4.push({
+              name: dev,
+              address: details.address
+            });
+            break;
+        }
+      }
+    });
+  }
+  return ifacesObj.ipv4[0].address;
+};
 const Koa = require('koa');
 const app = new Koa();
-
-// response
-app.use(ctx => {
-  ctx.body = new Date();
+const address = getLocalAddress();
+app.use(require('koa-static')('./client'));
+const server = app.listen(3000, address, function() {
+  console.log('\u001b[33m' + 'Available on:');
+  console.log('\u001b[32m' + '  http://' + address + ':3000/display.html' + '\u001b[0m');
+  console.log('Hit CTRL-C to stop the server');
 });
-const server = app.listen(3000, '10.0.1.54');
 const io = require('socket.io').listen(server);
-const data = {}
+const data = {};
 io.on('connection', (socket) => {
   console.log('connected count:' + socket.server.eio.clientsCount);
-  socket.on("positionFromClient", function(dataFromClient) {
-    if (data[socket.id] === void 0) data[socket.id] = {}
-    data[socket.id].position = {
-      x: dataFromClient.position_x,
-      y: dataFromClient.position_y
-    }
-  });
-  socket.on("rotationFromClient", function(dataFromClient) {
-    if (data[socket.id] === void 0) data[socket.id] = {}
-    data[socket.id].rotation = {
-      x: dataFromClient.rotation_x,
-      y: dataFromClient.rotation_y,
-      z: dataFromClient.rotation_z
-    }
-    console.log(data[socket.id].rotation);
+  socket.on("dataFromClient", function(dataFromClient) {
+    data["id-" + socket.id] = dataFromClient;
+    socket.broadcast.emit("data", data);
   });
   socket.on('disconnect', function() {
     console.log('disconnected count:' + socket.server.eio.clientsCount);
-    delete data[socket.id];
+    delete data["id-" + socket.id];
+    socket.broadcast.emit("disconnectClientID", "id-" + socket.id);
   });
 });
